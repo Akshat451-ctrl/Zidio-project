a9c427d8438f56b1123829173b7b05af397f7ce1
Akshat451-ctrl <akshatghatiya96@gmail.com>
2025-02-03 01:06:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Authform from './Authform';
+
+function App() {
+  return (
+    <div>
+      <Authform />
+    </div>
+  );
+}
+console.log("app is working");
+
+export default App;
